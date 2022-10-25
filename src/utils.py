7f27da0dd3e6d2bd82d0f998e0e2ebec250b528e
Alberto Becerra <alberto.becerra.tome@gmail.com>
2022-10-25 21:18:59 +0200

    Hotfix: doble igualdad en la asignacion de out

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ def get_coordinate(coord_name, exit_key, size):
             break
         except:
             if x.lower() == "q":
-                out == "q"
+                out = "q"
                 break
             else:
                 x = input(f"Introduzca coordenada {coord_name} entre 0 y {size - 1} o {exit_key} para salir")
