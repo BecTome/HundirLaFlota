63ca3f56e7eb177c2a110fb8ea94a13bd54be406
Alberto Becerra <alberto.becerra.tome@gmail.com>
2022-10-26 10:16:36 +0200

    parametrizacion exit key

--- src/utils.py
+++ src/utils.py
@@ -7,11 +7,13 @@ def get_coordinate(coord_name, exit_key, size):
             out = int(x)
             break
         except:
-            if x.lower() == "q":
-                out = "q"
+            if x.lower() == exit_key:
+                out = exit_key
                 break
             else:
                 x = input(f"Introduzca coordenada {coord_name} entre 0 y {size - 1} o {exit_key} para salir")
                 continue
     return out
-
+    
+if __name__ == "__main__":
+    get_coordinate("x", "q", 10)
